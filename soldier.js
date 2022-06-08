711000a4377df4518b60d89234cfd5e51a1b7be9
Ross <rabik95@bk.ru>
2022-06-08 09:50:18 +0300

    Доделан shoot. Сделан reload.

--- soldier.js
+++ soldier.js
@@ -11,8 +11,18 @@ const soldier = {
         if (this.gun.bullets > 0){
             this.gun.bullets - 1;
             console.log(бах-бах)
+        } else {
+            return 'Обойма пуста. Перезарядитесь.'
         }
     },
-    reload: function () {},
+    reload: function () {
+        if (this.bulletsHorn > 0){
+            this.bulletsHorn -= 1;
+            this.gun.bullets = 30
+            return "Перезарядка..."
+          } else {
+            return "Не осталось припасов"
+          }
+    },
     wound: function () {},
   };
